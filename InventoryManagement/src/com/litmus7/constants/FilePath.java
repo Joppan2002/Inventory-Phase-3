66c4fbf16cdcbb3866b2b815b9b9c016f783a8b7
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-09-02 18:50:19 +0530

    Create FilePath.java

---
+++ InventoryManagement/src/com/litmus7/constants/FilePath.java
@@ -0,0 +1,10 @@
+package com.litmus7.constants;
+
+public class FilePath 
+{
+	
+	public static final String INPUT_FOLDER = "C:\\Users\\ACER\\OneDrive\\Documents\\Litmus 7 assignments\\Java_Threads_Assignment_Phase3\\input";
+    public static final String PROCESSED_FOLDER = "C:\\Users\\ACER\\OneDrive\\Documents\\Litmus 7 assignments\\Java_Threads_Assignment_Phase3\\processed";
+    public static final String ERROR_FOLDER = "C:\\Users\\ACER\\OneDrive\\Documents\\Litmus 7 assignments\\Java_Threads_Assignment_Phase3\\error";
+
+}
