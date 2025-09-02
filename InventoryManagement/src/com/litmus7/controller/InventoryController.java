5df5485104ee0d3ff0d9c6ffcf7aef16ce11d589
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-09-02 18:45:54 +0530

    Create InventoryController.java

---
+++ InventoryManagement/src/com/litmus7/controller/InventoryController.java
@@ -0,0 +1,45 @@
+package com.litmus7.controller;
+
+import java.io.File;
+import java.util.concurrent.*;
+
+
+import com.litmus7.services.InventoryService;
+
+public class InventoryController {
+
+    private final InventoryService service = new InventoryService();
+
+    
+    public int triggerPhase3Processing() {
+        File[] files = service.getCsvFiles();
+        if (files == null || files.length == 0) {
+            System.out.println("No CSV files found in input folder.");
+            return 0;
+        }
+
+        int processedCount = 0;;
+
+        
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (File file : files) {
+            executor.submit(() -> {
+                boolean result = service.processSingleFile(file);
+            });
+            processedCount++;
+        }
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        
+        
+
+        return processedCount;
+    }
+}
