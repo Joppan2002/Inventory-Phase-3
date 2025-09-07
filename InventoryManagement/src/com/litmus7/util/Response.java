d513020a30a066796586fb2019f576051e30a926
Augustine Joel Joseph <115809487+Joppan2002@users.noreply.github.com>
2025-09-07 22:16:36 +0530

    Add files via upload

---
+++ InventoryManagement/src/com/litmus7/util/Response.java
@@ -0,0 +1,32 @@
+package com.litmus7.util;
+
+public class Response<T,U,V> 
+{
+	
+	T data;
+	U applicationStatus;
+	V message;
+	
+	public Response(T data,U applicationStatus,V message)
+	{
+		this.data=data;
+		this.applicationStatus=applicationStatus;
+		this.message=message;
+	}
+	
+	public T getData()
+	{
+		return this.data;
+	}
+	
+	public U getApplicationStatus()
+	{
+		return this.applicationStatus;
+	}
+	
+	public V getMessage()
+	{
+		return this.message;
+	}
+
+}
